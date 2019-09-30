e4bd1ccbcf2f1de7e30def7a0c39726652e83c1c
Keyvan <k.kholghi@gmail.com>
2019-09-30 23:55:48 +0000

    Generate a Random ShortURL

--- express_server.js
+++ express_server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = function () {
+  return Math.random().toString(36).substring(2, 8);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,10 +41,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  // let shortURL = req.params.shortURL;
-  // let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]/* What goes here? */ }
-  // res.render("urls_show", templateVars);
   res.render("urls_show", { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]})
 });
 
